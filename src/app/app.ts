7e2fd51a8cf4df4ead32c18013448f8ecef4f0a4
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2026-04-28 17:00:42 -0400

    feat: implement flowbite styles to main component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  //imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('sunshine-frontend');
+
+  constructor() {
+    initFlowbite();
+  }
 }
