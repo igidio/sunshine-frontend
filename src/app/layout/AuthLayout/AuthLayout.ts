f038aded504cb437ffba619f7c56bf367bdcacd8
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2026-04-28 22:29:58 -0400

    feat: create auth layout component

---
+++ src/app/layout/AuthLayout/AuthLayout.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-layout',
+  imports: [],
+  templateUrl: './AuthLayout.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class AuthLayout {}
