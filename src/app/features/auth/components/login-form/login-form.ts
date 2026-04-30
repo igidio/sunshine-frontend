42dd97a931d90c1b26e451a0f218914045ae35b5
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2026-04-29 22:13:20 -0400

    fix(auth): set correct properties to components

--- src/app/features/auth/components/login-form/login-form.ts
+++ src/app/features/auth/components/login-form/login-form.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { UiInput } from '../../../../shared/ui/ui-input/ui-input';
 import { UiField } from '../../../../shared/ui/ui-field/ui-field';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { UiCheckbox } from '../../../../shared/ui/ui-checkbox/ui-checkbox';
 
 @Component({
   selector: 'login-form',
-  imports: [UiInput, UiField, ReactiveFormsModule],
+  imports: [UiInput, UiField, ReactiveFormsModule, UiCheckbox],
   templateUrl: './login-form.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -13,5 +14,6 @@ export class LoginForm {
   form = new FormGroup({
     username_or_email: new FormControl(''),
     password: new FormControl(''),
+    remember_me: new FormControl(true),
   });
 }
