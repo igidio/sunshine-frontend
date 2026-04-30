b4dcc2b7f34fa36668ad2f832876920900afb5d9
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2026-04-29 23:11:11 -0400

    fix(ui): change some input names

--- src/app/features/auth/components/login-form/login-form.ts
+++ src/app/features/auth/components/login-form/login-form.ts
@@ -3,10 +3,12 @@ import { UiInput } from '../../../../shared/ui/ui-input/ui-input';
 import { UiField } from '../../../../shared/ui/ui-field/ui-field';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { UiCheckbox } from '../../../../shared/ui/ui-checkbox/ui-checkbox';
+import { UiButton } from '../../../../shared/ui/ui-button/ui-button';
+import { UiCard } from '../../../../shared/ui/ui-card/ui-card';
 
 @Component({
   selector: 'login-form',
-  imports: [UiInput, UiField, ReactiveFormsModule, UiCheckbox],
+  imports: [UiInput, UiField, ReactiveFormsModule, UiCheckbox, UiButton, UiCard],
   templateUrl: './login-form.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
