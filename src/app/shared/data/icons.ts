1e3c57097f3d13f9593ad714c5f8f9d694360762
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2026-05-04 22:55:16 -0400

    chore: install logos icon set

--- src/app/shared/data/icons.ts
+++ src/app/shared/data/icons.ts
@@ -1,6 +1,8 @@
 export const available_icons = {
   eye: 'icon-[ri--eye-line]',
   'eye-off': 'icon-[ri--eye-off-line]',
+  login: 'icon-[ri--login-box-fill]',
+  google: 'icon-[logos--google-icon]',
 } as const;
 
 export type IconValue = keyof typeof available_icons;
