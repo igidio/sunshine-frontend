d9641c6f13e1061b57525670f18d8f7adac8aea6
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2026-05-04 22:01:04 -0400

    refactor(ui): update ui-button and ui-field structure

--- src/app/shared/ui/ui-button/ui-button.ts
+++ src/app/shared/ui/ui-button/ui-button.ts
@@ -1,13 +1,18 @@
 import { Component, input, ChangeDetectionStrategy, booleanAttribute } from '@angular/core';
 import { ControlContainer, FormGroupDirective, ReactiveFormsModule } from '@angular/forms';
 import { ui_button_colors } from './ui-button-colors';
-import { UiVariants } from '../../data/ui-variants';
+import { UiSizes, UiVariants } from '../../data/ui-types';
+import { IconValue } from '../../data/icons';
+import { UiIcon } from '../ui-icon/ui-icon';
+import { NgClass } from '@angular/common';
+import { ui_button_sizes } from './ui-button-sizes';
+import { get_ui_classes, pick_classes } from '../../helpers/get-ui-classes';
 
 @Component({
   selector: 'ui-button',
   templateUrl: './ui-button.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, UiIcon, NgClass],
   viewProviders: [
     {
       provide: ControlContainer,
@@ -16,18 +21,30 @@ import { UiVariants } from '../../data/ui-variants';
   ],
 })
 export class UiButton {
-  a_type = input<'submit' | 'button' | 'reset'>('button');
-  a_label = input.required<string>();
-  a_id = input<string>('default-button');
+  ui_button_sizes = ui_button_sizes;
+
+  _type = input<'submit' | 'button' | 'reset'>('button');
+  _label = input<string | null>();
+  _id = input<string>('default-button');
+  icon = input<IconValue | null>();
   variant = input<UiVariants>('default');
+  size = input<UiSizes>('md');
   block = input(false, {
     transform: booleanAttribute,
   });
 
-  get variantClasses() {
+  get variant_classes() {
     const variant = ui_button_colors[this.variant()] ?? ui_button_colors.default;
-    return [variant.background, variant.text, variant.border, variant.hover, variant.focus]
-      .filter(Boolean)
-      .join(' ');
+    return get_ui_classes(variant);
+  }
+
+  get size_classes() {
+    const size = ui_button_sizes[this.size()];
+
+    return {
+      ...size,
+      padding_full: pick_classes(size, ['p']),
+      padding_xy: pick_classes(size, ['px', 'py']),
+    };
   }
 }
