868918c1f6ff77a2dbe2cd1f1b0c0403a8c2eb94
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2026-05-05 19:07:54 -0400

    feat: add environment configuration and dotenv support

---
+++ src/scripts/set-envs.js
@@ -0,0 +1,22 @@
+const { writeFileSync, mkdirSync } = require('fs');
+
+require('dotenv').config();
+
+const target_path = process.argv[2] || './src/environments/environment.ts';
+const target_path_dev = './src/environments/environment.development.ts';
+
+const server_url = process.env['SERVER_URL'] || 'http://localhost:3000';
+
+if ( !server_url) {
+  throw new Error('SERVER_URL environment variable is not set');
+}
+
+const env_file_content = `
+export const environment = {
+  serverUrl: '${server_url}',
+};
+`
+mkdirSync('./src/environments', { recursive: true });
+
+writeFileSync(target_path, env_file_content);
+writeFileSync(target_path_dev, env_file_content);
