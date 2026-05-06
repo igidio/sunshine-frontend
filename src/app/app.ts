4ee2ba5ed8f9ab6e3f83085b23443245161a7372
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2026-05-05 21:16:14 -0400

    refactor: move modal component into app template

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,13 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { UiToast } from './shared/ui/ui-toast/ui-toast';
+import { UiModal } from './shared/ui/ui-modal/ui-modal';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [RouterOutlet, UiToast],
+  imports: [RouterOutlet, UiToast, UiModal],
 })
 export class App {
   protected readonly title = signal('sunshine-frontend');
