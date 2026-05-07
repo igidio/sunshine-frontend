1cfbc0d3de8ee84f26e6ea9c710a6acef5edd62e
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2026-05-07 10:03:29 -0400

    feat(dashboard): create navbar componen

---
+++ src/app/features/dashboard/components/dashboard-navbar/dashboard-navbar.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { UiLogo } from '@/app/shared/ui/ui-logo/ui-logo';
+import { UiButton } from '@/app/shared/ui/ui-button/ui-button';
+import { UiMode } from '@/app/shared/ui/ui-mode/ui-mode';
+
+@Component({
+  selector: 'dashboard-navbar',
+  imports: [UiLogo, UiButton, UiMode],
+  templateUrl: './dashboard-navbar.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DashboardNavbar {}
