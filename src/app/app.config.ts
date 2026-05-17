d1874f0e5d1fe775d9682b4d434bc33fbdb21d64
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2026-05-16 23:29:06 -0400

    feat(core): add spanish locale support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,22 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 import { errorInterceptor } from './core/interceptors/error.interceptor';
+import localeEs from '@angular/common/locales/es-MX';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEs, 'es');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(withFetch(), withInterceptors([authInterceptor, errorInterceptor])),
     provideRouter(routes),
+    {
+      provide: LOCALE_ID,
+      useValue: 'es',
+    },
   ],
 };
