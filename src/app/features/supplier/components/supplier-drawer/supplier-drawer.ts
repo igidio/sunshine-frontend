986dee23ef5da87bf7c0af19c3493f7f97cfa988
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2026-05-19 21:11:57 -0400

    feat(supplier): create drawer component

---
+++ src/app/features/supplier/components/supplier-drawer/supplier-drawer.ts
@@ -0,0 +1,62 @@
+import { SupplierService } from '@/app/features/dashboard/services/supplier.service';
+import { SupplierInterface } from '@/app/shared/interfaces/supplier.interface';
+import { DrawerService } from '@/app/shared/services/drawer.service';
+import { ChangeDetectionStrategy, Component, inject, TemplateRef, viewChild } from '@angular/core';
+
+@Component({
+  selector: 'supplier-drawer',
+  imports: [],
+  templateUrl: './supplier-drawer.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SupplierDrawer {
+  supplierService = inject(SupplierService);
+  drawerService = inject(DrawerService);
+  template_drawer_create = viewChild.required<TemplateRef<any>>('drawer_create_content');
+  template_drawer_update = viewChild.required<TemplateRef<any>>('drawer_update_content');
+
+  ngAfterViewInit() {
+    this.open_drawer_create();
+  }
+
+  open_drawer_create() {
+    this.supplierService.selected_supplier.set(null);
+    this.drawerService.set_header({
+      title: 'Editar proveedor',
+      show_close_button: true,
+      show_divider: true,
+    });
+    this.drawerService.set_content(this.template_drawer_create());
+    this.drawerService.set_footer([
+      {
+        label: 'Cerrar',
+        variant: 'secondary',
+        size: 'sm',
+        action: () => {
+          this.drawerService.close();
+        },
+      },
+      {
+        label: 'Guardar',
+        variant: 'success',
+        size: 'sm',
+        action: () => {
+          // Acción de guardar
+        },
+      },
+    ]);
+    this.drawerService.open();
+  }
+
+  open_drawer_update(supplier: SupplierInterface) {
+    console.log('sadsad');
+
+    this.supplierService.selected_supplier.set(supplier);
+    this.drawerService.set_header({
+      title: 'Editar proveedor',
+      show_close_button: true,
+    });
+    this.drawerService.set_content(this.template_drawer_update());
+    this.drawerService.open();
+  }
+}
