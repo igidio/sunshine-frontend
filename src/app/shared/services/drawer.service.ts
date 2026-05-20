02284ff7bf554403fac8d7a45a72312f9f5469af
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2026-05-19 21:08:59 -0400

    feat(ui): enhance drawer component with footer support

--- src/app/shared/services/drawer.service.ts
+++ src/app/shared/services/drawer.service.ts
@@ -1,20 +1,29 @@
 import { computed, Injectable, signal, TemplateRef } from '@angular/core';
 import { Drawer } from 'flowbite';
+import { UiSizes, UiVariants } from '../data/ui-types';
+
+interface DrawerFooterButton {
+  label: string;
+  action: () => void;
+  variant: UiVariants;
+  size: UiSizes;
+}
 
 interface DrawerHeaderProperties {
   title: string;
   show_close_button?: boolean;
+  show_divider?: boolean;
 }
 
 interface DrawerSection<T = void> {
   template?: TemplateRef<any>;
-  show_divider?: boolean;
   properties?: T;
 }
 
 interface DrawerOptions {
   header?: DrawerSection<DrawerHeaderProperties>;
   content?: Omit<DrawerSection, 'show_divider' | 'properties'>;
+  footer?: DrawerSection<DrawerFooterButton[]>;
 }
 
 @Injectable({
@@ -26,6 +35,7 @@ export class DrawerService {
 
   register_drawer(drawer_instance: Drawer) {
     this.drawer = drawer_instance;
+    this.drawer.isVisible();
   }
 
   set_header(template: TemplateRef<any> | DrawerHeaderProperties) {
@@ -53,6 +63,24 @@ export class DrawerService {
         template,
       },
     }));
+  }
+
+  set_footer(template: TemplateRef<any> | DrawerFooterButton[]) {
+    if (template instanceof TemplateRef) {
+      this.options.update((options) => ({
+        ...options,
+        footer: {
+          template,
+        },
+      }));
+    } else {
+      this.options.update((options) => ({
+        ...options,
+        footer: {
+          properties: template,
+        },
+      }));
+    }
   }
 
   open() {
@@ -84,5 +112,9 @@ export class DrawerService {
 
   show_content = computed(() => {
     return !!this.options().content?.template;
+  });
+
+  show_footer = computed(() => {
+    return !!this.options().footer?.properties?.length || !!this.options().footer?.template;
   });
 }
--- src/app/shared/ui/ui-drawer/ui-drawer.ts
+++ src/app/shared/ui/ui-drawer/ui-drawer.ts
@@ -8,28 +8,32 @@ import {
   input,
   viewChild,
 } from '@angular/core';
-import { NgTemplateOutlet } from '@angular/common';
+import { NgClass, NgTemplateOutlet } from '@angular/common';
 import { Drawer, DrawerOptions, initDrawers } from 'flowbite';
 import { DrawerService } from '../../services/drawer.service';
 import { UiButton } from '../ui-button/ui-button';
+import { UiIcon } from '../ui-icon/ui-icon';
 
 @Component({
   selector: 'ui-drawer',
   templateUrl: './ui-drawer.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [NgTemplateOutlet, UiButton],
+  imports: [NgTemplateOutlet, UiButton, NgClass],
 })
 export class UiDrawer implements AfterViewInit {
   public drawer_service = inject(DrawerService);
   drawer_ref = viewChild<ElementRef>('drawer');
 
-  ngAfterViewInit() {
+  constructor() {
     initDrawers();
+  }
+
+  ngAfterViewInit() {
     const native_element = this.drawer_ref()?.nativeElement;
 
     if (native_element) {
       const drawer = new Drawer(native_element, {
-        placement: 'bottom',
+        placement: 'right',
       });
       this.drawer_service.register_drawer(drawer);
     }
