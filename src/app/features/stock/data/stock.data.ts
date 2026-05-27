7d70d65a3499d6446aeed741e1bdf2a32c373ad1
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2026-05-27 14:17:40 -0400

    feat(stock): create stock data component

---
+++ src/app/features/stock/data/stock.data.ts
@@ -0,0 +1,32 @@
+export const movement_types = {
+  purchase: {
+    value: 'purchase',
+    color: 'success',
+    label: 'Adquisición',
+  },
+  expired: {
+    value: 'expired',
+    color: 'danger',
+    label: 'Vencido',
+  },
+  damaged: {
+    value: 'damaged',
+    color: 'danger',
+    label: 'Dañado',
+  },
+  lost: {
+    value: 'lost',
+    color: 'danger',
+    label: 'Perdido',
+  },
+  adjustment: {
+    value: 'adjustment',
+    color: 'warning',
+    label: 'Ajuste',
+  },
+  internal_use: {
+    value: 'internal_use',
+    color: 'warning',
+    label: 'Uso Interno',
+  },
+};
