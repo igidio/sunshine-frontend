60c145efde62f0337aee8efbd6dcd4ffadda6ed9
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2026-06-02 14:33:47 -0400

    feat(sale): create sale components

---
+++ src/app/features/sale/components/sale-create/sale-create.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+    selector: 'sale-create',
+    template: '',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SaleCreate { }
