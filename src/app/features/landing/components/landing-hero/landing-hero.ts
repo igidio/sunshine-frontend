60ee8da005c4e341a813e5721a2b196f00106697
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2026-06-13 23:46:19 -0400

    feat(landing): create hero component

---
+++ src/app/features/landing/components/landing-hero/landing-hero.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ScrollRevealDirective } from '../../directives/scroll-reveal.directive';
+import { UiBadge } from "@/app/shared/ui/ui-badge/ui-badge";
+import { UiLogo } from "@/app/shared/ui/ui-logo/ui-logo";
+import { UiButton } from "@/app/shared/ui/ui-button/ui-button";
+import { UiIcon } from "@/app/shared/ui/ui-icon/ui-icon";
+
+@Component({
+  selector: 'landing-hero',
+  imports: [RouterLink, ScrollRevealDirective, UiLogo, UiButton, UiIcon],
+  templateUrl: './landing-hero.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LandingHero { }
