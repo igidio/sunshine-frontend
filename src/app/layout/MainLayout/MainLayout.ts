695a96effedb8f9a723f287079b5fffb1342a20b
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2026-06-14 11:37:16 -0400

    feat(main): implement header and footer to layout

--- src/app/layout/MainLayout/MainLayout.ts
+++ src/app/layout/MainLayout/MainLayout.ts
@@ -1,9 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MainHeader } from '../../features/landing/components/main-header/main-header';
+import { MainFooter } from '../../features/landing/components/main-footer/main-footer';
 
 @Component({
   selector: 'main-layout',
-  imports: [],
+  imports: [RouterOutlet, MainHeader, MainFooter],
   templateUrl: './MainLayout.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MainLayout {}
+export class MainLayout { }
