468f0e87085f48b33397efd8617c2c3f9286864b
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2026-06-14 11:38:12 -0400

    feat(landing): implement components to main page

--- src/app/features/landing/pages/main/main.ts
+++ src/app/features/landing/pages/main/main.ts
@@ -1,9 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { LandingHero } from '../../components/landing-hero/landing-hero';
+import { LandingProducts } from '../../components/landing-products/landing-products';
+import { LandingAbout } from '../../components/landing-about/landing-about';
+import { LandingLocation } from '../../components/landing-location/landing-location';
+import { LandingCta } from '../../components/landing-cta/landing-cta';
 
 @Component({
   selector: 'app-landing-main',
-  imports: [],
+  imports: [LandingHero, LandingProducts, LandingAbout, LandingLocation, LandingCta],
   templateUrl: './main.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class LandingMain {}
+export default class LandingMain { }
